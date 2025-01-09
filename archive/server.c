8eeb8f7ab9c937ac4526d908ed933d0d7a14d6c3
Aryan Mahawar <aryanmahawar205@gmail.com>
2025-01-09 20:57:58 +0530

    archive server

---
+++ archive/server.c
@@ -0,0 +1,69 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+void decrypt(char* message, int shift) {
+    for (int i = 0; message[i] != '\0'; i++) {
+        if (message[i] >= 'a' && message[i] <= 'z') {
+            message[i] = ((message[i] - 'a' - shift + 26) % 26) + 'a';
+        } else if (message[i] >= 'A' && message[i] <= 'Z') {
+            message[i] = ((message[i] - 'A' - shift + 26) % 26) + 'A';
+        }
+    }
+}
+
+int main(int argc, char const* argv[])
+{
+    int server_fd, new_socket;
+    ssize_t valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    socklen_t addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char* response = "Message received and decrypted";
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    valread = read(new_socket, buffer, sizeof(buffer) - 1);
+    buffer[valread] = '\0';
+    printf("Encrypted message received: %s\n", buffer);
+
+    decrypt(buffer, 4);
+    printf("Decrypted message: %s\n", buffer);
+
+    send(new_socket, response, strlen(response), 0);
+    printf("Response sent\n");
+
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
+
